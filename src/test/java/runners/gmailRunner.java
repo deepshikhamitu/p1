2520a249f1d49aad4d88f01414288d6070673833
deepshikhamitu <56016657+deepshikhamitu@users.noreply.github.com>
2025-04-27 10:52:03 +1000

    Update gmailRunner.java

--- src/test/java/runners/gmailRunner.java
+++ src/test/java/runners/gmailRunner.java
@@ -2,7 +2,7 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+import org.junit.runner.RunWith;
 @CucumberOptions(features="src/test/resources/features/gmailLogin.feature",glue="stepDefinitions",monochrome=true,plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json"})
 
 public class gmailRunner extends AbstractTestNGCucumberTests {
