f31677ef06ab3f78dc6d00c25c84159faf9c30db
deepshikhamitu <56016657+deepshikhamitu@users.noreply.github.com>
2025-04-27 13:33:12 +1000

    Update TestNGTestRunner.java

--- src/test/java/runners/TestNGTestRunner.java
+++ src/test/java/runners/TestNGTestRunner.java
@@ -4,7 +4,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/resources/features/google.feature",glue="stepDefinitions",monochrome=true,plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json"})
+@CucumberOptions(features="src/test/resources/features/google.feature",glue="stepDefinitions",monochrome=true,plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json","junit:target/cucumber-reports/Cucumber.xml"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 }
