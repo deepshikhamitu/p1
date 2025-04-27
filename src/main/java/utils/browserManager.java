969969f0b4f9710ee10e26e1e8da3652950b4a93
deepshikhamitu <56016657+deepshikhamitu@users.noreply.github.com>
2025-04-27 17:13:36 +1000

    Update browserManager.java

--- src/main/java/utils/browserManager.java
+++ src/main/java/utils/browserManager.java
@@ -2,7 +2,8 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import locators.locatorsManager;
 
 public class browserManager {
@@ -10,8 +11,15 @@ public class browserManager {
 	public locatorsManager locatorManager;
 	public chromeWebdriver bm;
 	public browserManager(){
-		
-		locatorManager=new locatorsManager(new chromeWebdriver().wbdriver());
+		private static WebDriver driver;
+		String a=System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver",a+"/driversfolder/chromeFolder/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		//driver.get("https://www.google.com");
+		driver.get("https://accounts.google.com");
+		driver.manage().window().maximize();
+		locatorManager=new locatorsManager(driver);
+		//locatorManager=new locatorsManager(new chromeWebdriver().wbdriver());
 	}
 	 
 	
